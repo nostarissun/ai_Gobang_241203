b08b37d6be608eb4928f8b334ecf4482472fae86
zmh <13562673623@163.com>
2024-12-03 18:56:54 +0800

    0.1

---
+++ test/src/main/java/org/example/Game.java
@@ -0,0 +1,379 @@
+package org.example;
+
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+
+
+import java.io.IOException;
+
+
+import java.io.OutputStream;
+
+import java.net.Socket;
+
+
+
+
+
+//0 无棋子
+//1 黑棋
+//2 白棋
+
+
+public class Game {
+    public static final int ALL_WIDTH = 800;
+    public static final int ALL_HEIGHT = 650;
+    public static final int cellsize = 30;
+    public static final int begin_x = 20;
+    public static final int begin_y = 20;
+    public static final int end_x = 590;
+    public static final int end_y = 590;
+    public static final int WIDTH = end_x - begin_x;
+    public static final int HEIGHT = end_y - begin_y;
+    public static final int x_cnt = WIDTH / cellsize;
+    public static final int y_cnt = HEIGHT / cellsize;
+    public static final int board_size_W = WIDTH / cellsize + 1;
+    public static final int board_size_H = HEIGHT / cellsize + 1;
+    public static int chess_board[][] = new int[board_size_H][board_size_W];
+    // public static int cur_chess = 1;
+    public void open(){
+        // System.out.println(board_size_H * board_size_W);
+        JFrame jf = new JFrame("五子棋");
+        Container contain = jf.getContentPane();
+        
+        DrawChessBoard boardPanel = new DrawChessBoard();
+        
+        
+ 
+
+        PlaceChess pc = new PlaceChess(chess_board);
+        
+
+
+        contain.add(boardPanel);
+        contain.add(pc);
+
+        
+        
+        
+        jf.setSize(ALL_WIDTH,ALL_HEIGHT);
+        jf.setLocation(320,240);
+        
+        jf.setVisible(true);
+        jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+
+
+   
+}
+class DrawChessBoard extends JPanel {
+    DrawChessBoard(){
+        setBackground(new Color(239, 226, 146));
+        
+    }
+    @Override
+    protected void paintComponent(Graphics g) {
+   
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+
+        // 设置绘制线条的属性
+        g2d.setStroke(new BasicStroke(1));
+        g2d.setColor(Color.BLACK);
+      
+        //画横线
+        for(int i = 0; i <= Game.y_cnt; i++)
+        {
+            int x0 = Game.begin_x;
+            int y0 = i * Game.cellsize + Game.begin_y;
+            int x1 = Game.end_x;
+            int y1 = y0;
+            g2d.drawLine(x0, y0, x1, y1);
+        }
+        //画竖线
+        for(int i = 0; i <= Game.x_cnt; i++)
+        {
+            int y0 = Game.begin_y;
+            int x0 = i * Game.cellsize + Game.begin_x;
+            int y1 = Game.end_y;
+            int x1 = x0;
+            g2d.drawLine(x0, y0, x1, y1);
+        }
+    }
+
+
+}
+
+
+
+class PlaceChess extends DrawChessBoard{
+    int chess_board[][] = new int[Game.board_size_H][Game.board_size_W];
+    int cur = 1;
+    boolean gamerun = true;
+
+     // 创建一个线程池，用于执行各种异步和多线程任务
+    
+
+    PlaceChess(int[][] cb){
+        // super(cur);
+     
+        
+            for(int i = 0; i < Game.board_size_H; i++){
+                for(int j = 0; j < Game.board_size_W; j++){
+                    this.chess_board[i][j] = cb[i][j];
+                }
+            }
+            
+                    
+            
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) 
+            {
+                if (gamerun && cur == 1) 
+                {
+                    // 提交MouseClick相关的耗时操作到线程池执行
+           
+                        MouseClick(e);
+                  
+                } 
+                
+                else if (gamerun && cur == 2) {
+                    // 提交异步任务到线程池执行
+                  
+                        int x = -1, y = -1;
+                        String serverName = "172.17.226.23";
+                        int servePort = 12000;
+                        byte[] buffer = new byte[1024];
+                        int bytesRead = 0;
+                        String modifiedSentence = "";
+
+                        try {
+                            // 创建套接字并连接到服务器
+                            Socket clientSocket = new Socket(serverName, servePort);
+
+                            String sentence = "";
+                            for (int i = 0; i < Game.board_size_H; i++) {
+                                for (int j = 0; j < Game.board_size_W; j++) {
+                                    sentence += chess_board[i][j];
+                                    
+                                }
+                                System.out.println();
+                            }
+                            for(int i = 0 ; i < Game.board_size_H; i++)
+                            {
+                                for(int j = 0; j < Game.board_size_W; j++)
+                                {
+                                    System.out.print(sentence.charAt(Game.board_size_W * i + j));
+                                }
+                                System.out.println();
+                            }
+                            
+
+                            // 向服务器发送数据
+                            OutputStream outputStream = clientSocket.getOutputStream();
+                            outputStream.write(sentence.getBytes());
+
+                            // 接收服务器返回的数据
+                            bytesRead = clientSocket.getInputStream().read(buffer);
+                            modifiedSentence = new String(buffer, 0, bytesRead);
+
+                            clientSocket.close();
+                            String[] newstr = modifiedSentence.split(",");
+                            x = Integer.parseInt(newstr[0]);
+                            y = Integer.parseInt(newstr[1]);
+                            if (x == -1 || y == -1) {
+                                System.exit(0);
+                            }
+
+
+                            chess_board[y][x] = 2;
+
+                            // 在UI线程上更新UI，确保线程安全
+                            
+                                 
+                                Graphics2D g2d = (Graphics2D) getGraphics();
+                                g2d.setColor(Color.WHITE);
+                                int cellSize = Game.cellsize;
+                                
+                                // (real_x - Game.begin_x) / Game.cellsize = t
+                                g2d.fillOval(x * cellSize - 10 + Game.begin_x, y * cellSize - 10 + Game.begin_y, 20, 20);
+    
+                                if (judge_win(cur)) {
+                                    gamerun = false;
+                                }
+                                System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!" + x + y);
+
+
+                        } catch (IOException a) {
+                            a.printStackTrace();
+                        }
+
+                
+                }
+                cur = cur % 2 + 1;
+            }
+        });
+    }
+
+    private void MouseClick(MouseEvent e) {
+        
+        int real_xlocation[] = new int[Game.board_size_W];
+        for(int i = 0; i < Game.board_size_W; i++){
+            real_xlocation[i] = Game.begin_x + Game.cellsize * i;
+        }
+
+        int real_ylocation[] = new int[Game.board_size_H];
+        for(int i = 0; i < Game.board_size_H; i++){
+            real_ylocation[i] = Game.begin_y + Game.cellsize * i;
+        }
+
+        if(judge_win(cur)){
+            gamerun = false;
+            return;
+        }
+        
+        int real_x = -1, real_y = -1;
+
+        
+        real_x = e.getX();
+        real_y = e.getY();
+
+        if(real_x > Game.end_x || real_x < Game.begin_x || real_y > Game.end_y || real_y < Game.begin_y){
+            System.out.println("鼠标点击不合法");
+            return;
+        }
+
+        real_x = get_min_location(real_xlocation, real_x, Game.board_size_W);
+        real_y = get_min_location(real_ylocation, real_y, Game.board_size_H);
+        
+        // System.out.println(real_x);
+        // System.out.println(real_y);
+
+        int board_x = (real_x - Game.begin_x) / Game.cellsize;
+        int board_y = (real_y - Game.begin_y) / Game.cellsize;
+        // if(chess_board[x][y] != 0)return;
+        if(chess_board[board_y][board_x] != 0) {
+            System.out.println("已有棋子");   
+            return;
+        }
+        Graphics2D g2d = (Graphics2D) getGraphics();
+        g2d.setColor(Color.BLACK);
+    
+        // System.out.println("\n\n\n" + this.cur);
+        chess_board[board_y][board_x] = 1;
+
+
+        g2d.fillOval(real_x - 10, real_y - 10, 20, 20);
+        
+        if(judge_win(cur))
+        {
+            gamerun = false;
+        }
+     
+
+        // put_to_ai(chess_board);
+        // System.out.println("\n\n\n" + this.cur);
+        
+        // cur = cur % 2 + 1;
+    }
+
+    int get_min_location(int []real, int cur, int size){
+        int res = 1000;
+        for(int i = 0; i < size; i++){
+            if(Math.abs(cur - real[i]) < Math.abs(res - cur))
+                res = real[i];
+        }
+        return res;
+    }
+
+    boolean judge_win(int chess){
+        // System.out.println("!!!!!!!!!!!!!!!");
+        int dir[][] = new int[8][2];
+           // 八个方向 顺时针         
+        dir[0] = new int[]{0, -1};
+        dir[1] = new int[]{1, -1};
+        dir[2] = new int[]{1, 0};
+        dir[3] = new int[]{1, 1};
+        dir[4] = new int[]{0, 1};
+        dir[5] = new int[]{-1, 1};
+        dir[6] = new int[]{-1, 0};
+        dir[7] = new int[]{-1, -1};
+
+
+        for(int i = 0; i < Game.board_size_H; i++)
+        {
+            for(int j = 0; j < Game.board_size_W; j++)
+            {
+                for(int k = 0; k < 8; k++)
+                {
+                    int dx = dir[k][0];
+                    int dy = dir[k][1];
+                    int cnt = 0;
+                    for(int m = 0; m < 5; m++)
+                    {
+                        if(i + m * dx < Game.board_size_W && i + m * dx >= 0 && j + m * dy  < Game.board_size_H && j + m * dy >= 0 && chess_board[i + m * dx][j + m * dy] == chess)cnt++;
+                    }
+                    if(cnt == 5){
+                        if(cur == 1)
+                        show_win();
+                        // System.exit(0);
+                        else show_lose();
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    void show_win(){
+        JDialog dialog = new JDialog();
+        dialog.setTitle("THE FINAL RESULT:");
+        dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        dialog.setSize(300, 150);
+
+        dialog.setLocationRelativeTo(null);// 居中
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+
+        JLabel label = new JLabel("YOU WIN THE GAME!!!");
+        panel.add(label, BorderLayout.CENTER);
+
+        dialog.add(panel);
+        dialog.setVisible(true);
+    }
+
+    void show_lose(){
+        JDialog dialog = new JDialog();
+        dialog.setTitle("THE FINAL RESULT:");
+        dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        dialog.setSize(300, 150);
+
+        dialog.setLocationRelativeTo(null);// 居中
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+
+        JLabel label = new JLabel("YOU LOSE THE GAME!!!");
+        panel.add(label, BorderLayout.CENTER);
+
+        dialog.add(panel);
+        dialog.setVisible(true);
+    }
+}
+
+
+
+
+
+
+
+    //     if(cur == 2 && gamerun){
+    //         
---
+++ test/src/main/java/org/example/Main.java
@@ -0,0 +1,183 @@
+package org.example;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
+
+
+public class Main {
+    public static final int X = 800;
+    public static final int Y = 650;
+    public static void main(String args[]){
+        JFrame jf = new JFrame("五子棋");
+        Container contain = jf.getContentPane();
+        
+        
+        jf.setSize(X,Y);
+        jf.setLocation(320,240);
+        
+
+        main_page(jf, contain);
+        
+        
+
+        jf.setVisible(true);
+        jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+    }
+public static void open_game(JFrame win, Container contain){
+    Game game = new Game();
+    game.open();
+
+}
+    public static void main_page(JFrame windows, Container contain){
+        
+        windows.setLayout(null); // 设置布局管理器为null
+
+        JLabel main_label = new JLabel("五子棋AI对战");
+        main_label.setFont(new Font("SansSerif", Font.BOLD, 18));
+        main_label.setBounds(X / 2 - 70, 0, 200, 30); // 设置标签的位置和大小
+        contain.add(main_label);
+
+        
+        JButton button1 = new JButton("游客登录");
+        JButton button2 = new JButton("登录");
+        JButton button3 = new JButton("注册");
+        JButton button4 = new JButton("退出游戏");
+        button1.setFont(new Font("SansSerif", Font.BOLD, 16));
+        button2.setFont(new Font("SansSerif", Font.BOLD, 16));
+        button3.setFont(new Font("SansSerif", Font.BOLD, 16));
+        button4.setFont(new Font("SansSerif", Font.BOLD, 16));
+        button1.setBounds(X / 2 - 70, Y / 3, 100, 30);
+        button2.setBounds(X / 2 - 70, Y / 3 + 30 + 50, 100, 30);
+        button3.setBounds(X / 2 - 70, Y / 3 + 110 + 50, 100, 30);
+        button4.setBounds(X / 2 - 70, Y / 3 + 190 + 50, 100, 30);
+        // ImageIcon icon1 = new ImageIcon("C://Users//联想//Desktop//ai五子棋课设//label.png"); // 替换为实际图标路径
+        // button1.setIcon(icon1);
+//         button1.setHorizontalTextPosition(SwingConstants.CENTER);
+// button1.setVerticalTextPosition(SwingConstants.BOTTOM);
+
+
+        windows.add(button1);
+        windows.add(button2);
+        windows.add(button3);
+        windows.add(button4);
+
+        button4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                int result = JOptionPane.showConfirmDialog(windows, "确定要退出游戏吗？", "退出确认", JOptionPane.YES_NO_OPTION);
+                if (result == JOptionPane.YES_OPTION) {
+                    System.exit(0); // 用户确认退出
+                }
+            }
+        });
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                open_game(windows, contain);
+            }
+        });
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                    login(windows, contain);    
+                    open_game(windows, contain);
+            }
+        });
+
+        button3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                    login(windows, contain);    
+            }
+        });
+
+    }
+
+    public static void register(JFrame windows, Container contain){
+        
+            JDialog get_info = new JDialog(windows, true);
+            get_info.setSize(X / 2, Y / 2 - 180);
+
+
+            get_info.getContentPane().setLayout(new BoxLayout(get_info.getContentPane(), BoxLayout.Y_AXIS));
+            // 创建输入组件
+            JLabel account = new JLabel("账号:");
+            JLabel pwd = new JLabel("密码:");
+            JTextField get_account = new JTextField(20);
+            JTextField get_pwd = new JTextField(20);
+            JButton ok = new JButton("确认");
+            get_info.add(account);
+            get_info.add(get_account);
+            get_info.add(pwd);
+            get_info.add(get_pwd);
+            get_info.add(ok);
+
+
+            
+            get_info.setLocationRelativeTo(windows); // 将对话框居中于父窗口
+            
+
+            ok.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e){
+                        String user = get_account.getText();
+                        String password = get_pwd.getText();
+                        int result = JOptionPane.showConfirmDialog(get_info, "账号: " + user + "\n密码: " + password + "\n确认登录？", "登录确认", JOptionPane.YES_NO_OPTION);
+                        if (result == JOptionPane.YES_OPTION) {
+                            get_info.dispose();  // 用户确认退出
+                        }
+                    }   
+                
+            });
+            get_info.setVisible(true);
+            get_info.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+         
+    
+    }
+
+    public static void login(JFrame windows, Container contain){
+        
+            JDialog get_info = new JDialog(windows, true);
+            get_info.setSize(X / 2, Y / 2 - 180);
+
+
+            get_info.getContentPane().setLayout(new BoxLayout(get_info.getContentPane(), BoxLayout.Y_AXIS));
+            // 创建输入组件
+            JLabel account = new JLabel("账号:");
+            JLabel pwd = new JLabel("密码:");
+            JTextField get_account = new JTextField(20);
+            JTextField get_pwd = new JTextField(20);
+            JButton ok = new JButton("确认");
+            get_info.add(account);
+            get_info.add(get_account);
+            get_info.add(pwd);
+            get_info.add(get_pwd);
+            get_info.add(ok);
+
+
+            
+            get_info.setLocationRelativeTo(windows); // 将对话框居中于父窗口
+            
+
+            ok.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e){
+                        String user = get_account.getText();
+                        String password = get_pwd.getText();
+                        int result = JOptionPane.showConfirmDialog(get_info, "账号: " + user + "\n密码: " + password + "\n确认登录？", "登录确认", JOptionPane.YES_NO_OPTION);
+                        if (result == JOptionPane.YES_OPTION) {
+                            get_info.dispose();  // 用户确认退出
+                        }
+                    }   
+                
+            });
+            get_info.setVisible(true);
+            get_info.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+         
+    }
+
+}
